47016a68636aa7cb90179cece1c23f27fad642bb
violetstar701 <violetstar701@gmail.com>
2023-04-20 10:31:57 +0300

    agent start

---
+++ funcs.py
@@ -0,0 +1,1 @@
+# Fetches latest crypto info from crypto
---
+++ tools.py
@@ -0,0 +1,25 @@
+from langchain.chat_models import ChatOpenAI
+from langchain.tools import BaseTool
+from langchain.utilities import GoogleSearchAPIWrapper, SerpAPIWrapper
+from langchain.agents import load_tools
+from dotenv import load_dotenv
+
+load_dotenv()
+
+llm = ChatOpenAI(temperature=0, model_name="gpt-4")
+
+
+class LatestInfoTool(BaseTool):
+    name = "LatestInfo"
+    description = "useful for providing latest information on crypto movements, news and prices"
+
+    def _run(self, query: str) -> str:
+        """Use the tool."""
+
+        search = SerpAPIWrapper()
+
+        return search.run(query)
+
+    async def _arun(self, query: str) -> str:
+        """Use the tool asynchronously."""
+        raise NotImplementedError("LatestInfoTool does not support async")
